a6731da6153bbd77dd7cff737da760a8a2b9e6a4
mcorcias <mosescorcias@gmail.com>
2019-10-22 17:40:23 +0300

    query avaivilty books

--- src/queries/borrow_q.js
+++ src/queries/borrow_q.js
@@ -1,7 +1,7 @@
 const dbConnection = require('../database/db_connection');
 
 const getData = (cb) => {
-    dbConnection.query('SELECT * FROM books', (err, res) => {
+    dbConnection.query("select b.isnb,b.title,b.author,bo.start_time,bo.end_time,(case when bo.end_time>current_date then 'available' else 'unavailable' end) as availability from books b join borrowing bo on b.isnb=bo.book_id ", (err, res) => {
         if (err) return cb(err);
         console.log('res.rows=:', res.rows);
         cb(null, res.rows);
